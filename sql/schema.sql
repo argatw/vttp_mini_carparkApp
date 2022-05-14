c96a9de957883bd1af4e3c96d3b81f7dc2dcd34c
unknown <arga173@yahoo.com.sg>
2022-05-14 22:00:35 +0800

    schema change for user creation

--- sql/schema.sql
+++ sql/schema.sql
@@ -5,19 +5,19 @@ create database cpDatabase;
 use cpDatabase;
 
 create table user (
-    userId varchar(64) PRIMARY KEY not null,
+    userId varchar(64) not null,
     password varchar(64) not null,
-    email varchar(64) not null
+    email varchar(64) PRIMARY KEY not null 
     -- primary key(userId);
 );
 
 create table carparkSearched ( 
     searchId int auto_increment,
-    userId varchar(64) not null, 
+    email varchar(64) not null, 
     development varchar(128) not null,
     availableLots int(5) not null,
 
     primary key(searchId),
-    constraint fk_userId
-    foreign key(userId) references user(userId)
+    constraint fk_email
+    foreign key(email) references user(email)
 );
